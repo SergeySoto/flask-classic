bcacde21fd89a950d0c00ef192e17dce5e8cf017
Sergey <sergey.soto@gmail.com>
2025-01-11 17:14:00 +0100

    agregado clase Conexion

--- registros_ig/models.py
+++ registros_ig/models.py
@@ -1,12 +1,12 @@
 import sqlite3
 from registros_ig import ORIGIN_DATA
+from registros_ig.conexion import Conexion
 
 def select_all():
-    conexion = sqlite3.connect(ORIGIN_DATA)
-    cur = conexion.cursor()
-    res = cur.execute('SELECT * from movements;')
-    filas = res.fetchall() #datos de columnas (1,2024-01-01,nomina enero, 1500)
-    columnas = res.description #nombres de columnas (id,00000)(date,00000)(concept,00000)
+
+    conectar = Conexion('select * from movements;')
+    filas = conectar.res.fetchall() #datos de columnas (1,2024-01-01,nomina enero, 1500)
+    columnas = conectar.res.description #nombres de columnas (id,00000)(date,00000)(concept,00000)
     
     lista_diccionario = []
     for f in filas:
@@ -16,30 +16,23 @@ def select_all():
             diccionario[c[0]] = f[posicion]
             posicion +=1
         lista_diccionario.append(diccionario)
-    conexion.close()
+    conectar.con.close()
 
     return lista_diccionario
 
 def insert(registroForm):
-    conexion = sqlite3.connect(ORIGIN_DATA)
-    cur = conexion.cursor()
-    cur.execute('insert into movements (date,concept,quantity) values(?,?,?);',registroForm)
-
-    conexion.commit()#Funcion para validar registro
-
-    conexion.close()
+    conectarInser = Conexion('insert into movements (date,concept,quantity) values(?,?,?);',registroForm)
+    conectarInser.con.commit()#Funcion para validar registro
+    conectarInser.con.close()
 
 def select_by(id):
-    conexion = sqlite3.connect(ORIGIN_DATA)
-    cur = conexion.cursor()
-    res = cur.execute(f'select * from movements where id={id};')
-    result = res.fetchall()
-    conexion.close()
+    conectarSelectBy = Conexion(f'select * from movements where id={id};')
+    result = conectarSelectBy.res.fetchall()
+    conectarSelectBy.con.close()
+
     return result[0]
 
 def delete_by(id):
-    conexion = sqlite3.connect(ORIGIN_DATA)
-    cur = conexion.cursor()
-    cur.execute(f'delete from movements where id={id};')
-    conexion.commit() #funcion para validar borrado
-    conexion.close()
+    conectarDeleteBy = Conexion(f'delete from movements where id={id};')
+    conectarDeleteBy.con.commit() #funcion para validar borrado
+    conectarDeleteBy.con.close() 
